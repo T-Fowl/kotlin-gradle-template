2675707b73cc47703c93f0926b133ce144c4071d
Thomas Fowler <T-Fowl@users.noreply.github.com>
2020-01-08 11:50:54 +1100

    Added arrow-kt bintray repository

--- buildSrc/src/main/kotlin/Repositories.kt
+++ buildSrc/src/main/kotlin/Repositories.kt
@@ -6,5 +6,6 @@ object Repositories {
         const val kotlinx = "https://dl.bintray.com/kotlin/kotlinx"
         const val ktor = "https://dl.bintray.com/kotlin/ktor"
         const val exposed = "https://dl.bintray.com/kotlin/exposed"
+        const val arrowkt = "https://dl.bintray.com/arrow-kt/arrow-kt"
     }
 }
