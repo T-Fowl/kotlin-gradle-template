d901a90c4fc47cde6301b8c374975dfe9457fa6e
Thomas Fowler <T-Fowl@users.noreply.github.com>
2020-02-07 21:08:09 +1100

    Added sonatype repositories

--- buildSrc/src/main/kotlin/Repositories.kt
+++ buildSrc/src/main/kotlin/Repositories.kt
@@ -1,11 +1,20 @@
 object Repositories {
 
     const val jitpack = "https://jitpack.io"
+    const val arrowkt = "https://dl.bintray.com/arrow-kt/arrow-kt"
+    
+    val sonatype = Sonatype
+
+    object Sonatype {
+        const val released = "https://oss.sonatype.org/content/repositories/releases/"
+        const val snapshots = "https://oss.sonatype.org/content/repositories/snapshots/"
+    }
+    
+    val kotlin = Kotlin
 
     object Kotlin {
         const val kotlinx = "https://dl.bintray.com/kotlin/kotlinx"
         const val ktor = "https://dl.bintray.com/kotlin/ktor"
         const val exposed = "https://dl.bintray.com/kotlin/exposed"
-        const val arrowkt = "https://dl.bintray.com/arrow-kt/arrow-kt"
     }
 }
